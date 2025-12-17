621c6188b155f6394dcc605ceefa479ad5327bbc
ultraelectronica <fyketonel@gmail.com>
2025-12-17 18:28:46 +0800

    Prevent app from appearing in recent apps by excluding from recents and finishing task on pause.

--- android/app/src/main/kotlin/com/example/first_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/first_project/MainActivity.kt
@@ -2,4 +2,11 @@ package com.example.first_project
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity()
+class MainActivity: FlutterFragmentActivity() {
+    
+    override fun onPause() {
+        super.onPause()
+        // Remove from recent apps when user leaves the app
+        finishAndRemoveTask()
+    }
+}
